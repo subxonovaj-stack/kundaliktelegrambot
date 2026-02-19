afb4c1539da850f7928e90ac5a3be85ef1c232c9
subxonovaj-stack <subxonovaj@gmail.com>
2026-02-19 15:44:01 +0900

    Updated bot file

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ print("BOT TOKEN EXISTS:", bool(BOT_TOKEN))
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+# 🔥 DELETE OLD WEBHOOK (VERY IMPORTANT)
+bot.remove_webhook()
+
 # Prevent double execution
 is_running = False
 
